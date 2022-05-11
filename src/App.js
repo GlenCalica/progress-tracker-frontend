8e5b16d226cb987aedc9aab9f018428e300d2adc
Glen Calica <gcalica@myseneca.ca>
2022-05-11 16:55:55 -0400

    added basic router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import "./App.css";
 import Navbar from "./Navbar";
 
+import Home from "./Home";
+import Metrics from "./Metrics";
+import Settings from "./Settings";
+import Profile from "./Profile";
+
 function App() {
    return (
-      <div className="App">
-         <Navbar></Navbar>
-         <main className="ml-64 p-4">
-            <p>test</p>
-         </main>
-      </div>
+      <BrowserRouter>
+         <div className="App">
+            <Navbar />
+            <main className="ml-64 p-4">
+               <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/metrics" element={<Metrics />} />
+                  <Route path="/profile" element={<Profile />} />
+                  <Route path="/settings" element={<Settings />} />
+               </Routes>
+            </main>
+         </div>
+      </BrowserRouter>
    );
 }
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import barchart from "./icons/bar-chart.svg";
 import person from "./icons/person.svg";
 import settings from "./icons/settings.svg";
@@ -7,18 +9,28 @@ export default function Menu() {
    return (
       <aside className="fixed top-0 bottom-0 left-0 w-64 p-4 bg-gray-400">
          <ul>
-            <li className="my-2 py-1 px-2">Progress Tracker</li>
-            <li className="flex my-2 py-1 px-2 rounded-md hover:bg-gray-500">
-               <img src={barchart} alt="metrics icon" className="h-6 px-2" />
-               <p>Metrics</p>
+            <li className="mb-2 py-1 px-2">
+               <Link to="/">
+                  <h1 className="text-lg">Progress Tracker</h1>
+               </Link>
             </li>
-            <li className="flex my-2 py-1 px-2 rounded-md hover:bg-gray-500">
-               <img src={person} alt="metrics icon" className="h-6 px-2" />
-               <p>Profile</p>
+            <li className="my-2 py-1 px-2 rounded-md hover:bg-gray-500">
+               <Link to="/metrics" className="flex">
+                  <img src={barchart} alt="metrics icon" className="h-6 px-2" />
+                  <p>Metrics</p>
+               </Link>
             </li>
-            <li className="flex my-2 py-1 px-2 rounded-md hover:bg-gray-500">
-               <img src={settings} alt="metrics icon" className="h-6 px-2" />
-               <p>Settings</p>
+            <li className="my-2 py-1 px-2 rounded-md hover:bg-gray-500">
+               <Link to="/profile" className="flex">
+                  <img src={person} alt="metrics icon" className="h-6 px-2" />
+                  <p>Profile</p>
+               </Link>
+            </li>
+            <li className="my-2 py-1 px-2 rounded-md hover:bg-gray-500">
+               <Link to="/settings" className="flex">
+                  <img src={settings} alt="metrics icon" className="h-6 px-2" />
+                  <p>Settings</p>
+               </Link>
             </li>
          </ul>
       </aside>
