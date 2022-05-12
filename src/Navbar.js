34830fd9f19527ca24367eb1f009eaeb5962fad6
Glen Calica <gcalica@myseneca.ca>
2022-05-11 22:42:47 -0400

    added more navbar styling

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import barchart from "./icons/bar-chart.svg";
 import person from "./icons/person.svg";
@@ -7,30 +7,30 @@ import settings from "./icons/settings.svg";
 
 export default function Menu() {
    return (
-      <aside className="fixed top-0 bottom-0 left-0 w-64 p-4 bg-slate-900">
+      <aside className="fixed top-0 bottom-0 left-0 w-64 p-4 bg-slate-900 border-r border-slate-600">
          <ul>
             <li className="mb-2 py-1 px-2">
-               <Link to="/">
+               <NavLink to="/">
                   <h1 className="text-lg text-gray-100">Progress Tracker</h1>
-               </Link>
+               </NavLink>
             </li>
-            <li className="my-2 py-1 px-2 rounded-md hover:bg-gray-500">
-               <Link to="/metrics" className="flex">
+            <li className="my-2 py-1 px-2 rounded-md hover:bg-slate-800">
+               <NavLink to="/metrics" className="flex">
                   <img src={barchart} alt="metrics icon" className="h-6 px-2" />
                   <p className="font-light text-gray-300">Metrics</p>
-               </Link>
+               </NavLink>
             </li>
-            <li className="my-2 py-1 px-2 rounded-md hover:bg-gray-500">
-               <Link to="/profile" className="flex">
+            <li className="my-2 py-1 px-2 rounded-md hover:bg-slate-800">
+               <NavLink to="/profile" className="flex">
                   <img src={person} alt="metrics icon" className="h-6 px-2" />
                   <p className="font-light text-gray-300">Profile</p>
-               </Link>
+               </NavLink>
             </li>
-            <li className="my-2 py-1 px-2 rounded-md hover:bg-gray-500">
-               <Link to="/settings" className="flex">
+            <li className="my-2 py-1 px-2 rounded-md hover:bg-slate-800 absolute inset-x-4 bottom-0">
+               <NavLink to="/settings" className="flex">
                   <img src={settings} alt="metrics icon" className="h-6 px-2" />
                   <p className="font-light text-gray-300">Settings</p>
-               </Link>
+               </NavLink>
             </li>
          </ul>
       </aside>
