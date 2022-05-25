f6ca43ca11a5014cd84cca5c85fecea40e03acdf
Glen Calica <gcalica@myseneca.ca>
2022-05-25 13:15:01 -0400

    added basic style to register page

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,14 +1,15 @@
 import { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
 
 export default function Register() {
    const [formData, setFormData] = useState({
       name: "",
       email: "",
       password: "",
-      password2: "",
+      confirmPassword: "",
    });
 
-   const { name, email, password, password2 } = formData;
+   const { name, email, password, confirmPassword } = formData;
 
    const onChange = (e) => {
       setFormData((prevState) => ({
@@ -22,49 +23,77 @@ export default function Register() {
    };
 
    return (
-      <>
-         <section>
-            <h1>Create an account</h1>
-         </section>
-         <section>
-            <form onSubmit={onSubmit}>
-               <div>
+      <main className="grid place-content-center h-screen">
+         <form
+            onSubmit={onSubmit}
+            className="h-full p-12 rounded-xl bg-slate-200"
+            style={{ width: "32rem" }}
+         >
+            <h1 className="text-3xl">Create an account</h1>
+            <div>
+               <div className="my-4">
+                  <label htmlFor="name">Name</label>
+                  <br />
                   <input
                      type="text"
                      id="name"
                      name="name"
                      value={name}
-                     placeholder="Enter your name"
                      onChange={onChange}
+                     className="w-full mt-1 p-2 rounded"
                   />
+               </div>
+
+               <div className="my-4">
+                  <label htmlFor="email">Email</label>
+                  <br />
                   <input
                      type="email"
                      id="email"
                      name="email"
                      value={email}
-                     placeholder="Enter your email"
                      onChange={onChange}
+                     className="w-full mt-1 p-2 rounded"
                   />
+               </div>
+
+               <div className="my-4">
+                  <label htmlFor="password">Password</label>
+                  <br />
                   <input
                      type="password"
                      id="password"
                      name="password"
                      value={password}
-                     placeholder="Enter your password"
                      onChange={onChange}
-                  />
-                  <input
-                     type="password2"
-                     id="password2"
-                     name="password2"
-                     value={password2}
-                     placeholder="Confirm password"
-                     onChange={onChange}
+                     className="w-full mt-1 p-2 rounded"
                   />
                </div>
-               <button type="submit">Submit</button>
-            </form>
-         </section>
-      </>
+
+               <div className="my-4">
+                  <label htmlFor="confirmPassword">Confirm Password</label>
+                  <br />
+                  <input
+                     type="confirmPassword"
+                     id="confirmPassword"
+                     name="confirmPassword"
+                     value={confirmPassword}
+                     onChange={onChange}
+                     className="w-full mt-1 p-2 rounded"
+                  />
+               </div>
+            </div>
+            <button
+               type="submit"
+               className="w-36 mt-4 mb-2 p-3 rounded bg-slate-400"
+            >
+               Sign Up
+            </button>
+            <br />
+            <NavLink to="/login" className="w-12 text-sm font-medium">
+               Already have an account?
+            </NavLink>
+         </form>
+      </main>
    );
 }
