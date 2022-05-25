1611cd4fc7289137042ff837895dff96d1db7c36
Glen Calica <gcalica@myseneca.ca>
2022-05-25 12:17:26 -0400

    added forms to login and register pages

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,3 +1,52 @@
+import { useState, useEffect } from "react";
+
 export default function Login() {
-   return <h1>Log In</h1>;
+   const [formData, setFormData] = useState({
+      email: "",
+      password: "",
+   });
+
+   const { email, password } = formData;
+
+   const onChange = (e) => {
+      setFormData((prevState) => ({
+         ...prevState,
+         [e.target.name]: e.target.value,
+      }));
+   };
+
+   const onSubmit = (e) => {
+      e.preventDefault();
+   };
+
+   return (
+      <>
+         <section>
+            <h1>Log in</h1>
+         </section>
+         <section>
+            <form onSubmit={onSubmit}>
+               <div>
+                  <input
+                     type="email"
+                     id="email"
+                     name="email"
+                     value={email}
+                     placeholder="Enter your email"
+                     onChange={onChange}
+                  />
+                  <input
+                     type="password"
+                     id="password"
+                     name="password"
+                     value={password}
+                     placeholder="Enter your password"
+                     onChange={onChange}
+                  />
+               </div>
+               <button type="submit">Submit</button>
+            </form>
+         </section>
+      </>
+   );
 }
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,3 +1,70 @@
+import { useState, useEffect } from "react";
+
 export default function Register() {
-   return <h1>Register</h1>;
+   const [formData, setFormData] = useState({
+      name: "",
+      email: "",
+      password: "",
+      password2: "",
+   });
+
+   const { name, email, password, password2 } = formData;
+
+   const onChange = (e) => {
+      setFormData((prevState) => ({
+         ...prevState,
+         [e.target.name]: e.target.value,
+      }));
+   };
+
+   const onSubmit = (e) => {
+      e.preventDefault();
+   };
+
+   return (
+      <>
+         <section>
+            <h1>Create an account</h1>
+         </section>
+         <section>
+            <form onSubmit={onSubmit}>
+               <div>
+                  <input
+                     type="text"
+                     id="name"
+                     name="name"
+                     value={name}
+                     placeholder="Enter your name"
+                     onChange={onChange}
+                  />
+                  <input
+                     type="email"
+                     id="email"
+                     name="email"
+                     value={email}
+                     placeholder="Enter your email"
+                     onChange={onChange}
+                  />
+                  <input
+                     type="password"
+                     id="password"
+                     name="password"
+                     value={password}
+                     placeholder="Enter your password"
+                     onChange={onChange}
+                  />
+                  <input
+                     type="password2"
+                     id="password2"
+                     name="password2"
+                     value={password2}
+                     placeholder="Confirm password"
+                     onChange={onChange}
+                  />
+               </div>
+               <button type="submit">Submit</button>
+            </form>
+         </section>
+      </>
+   );
 }
