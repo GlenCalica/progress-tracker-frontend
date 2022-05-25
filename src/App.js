a1a9c8b272692bc8a122ab4f0134b946cfe4030b
Glen Calica <gcalica@myseneca.ca>
2022-05-25 11:34:01 -0400

    added login and register pages

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ import Metrics from "./pages/Metrics";
 import Metric from "./pages/Metric";
 import Settings from "./pages/Settings";
 import Profile from "./pages/Profile";
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 import NotFound from "./pages/NotFound";
 
 function App() {
@@ -28,6 +32,8 @@ function App() {
             <Route path="/metric/:name" element={wrapNavbar(<Metric />)} />
             <Route path="/profile" element={wrapNavbar(<Profile />)} />
             <Route path="/settings" element={wrapNavbar(<Settings />)} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
             <Route path="*" element={<NotFound />} />
          </Routes>
       </BrowserRouter>
