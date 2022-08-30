4f63fb4c4782e5f60f91851a739f71e7a2332f37
Glen Calica <gcalica@myseneca.ca>
2022-08-29 23:43:02 -0400

    added info to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,43 @@ import Logout from "../components/Logout";
 
 export default function Home(props) {
    return (
-      <>
-         <h1>Home</h1>
+      <div className="home">
+         <h1>Welcome to Progress Tracker</h1>
+         <p>
+            My name is Glen Calica. I made this website for my portfolio. Feel
+            free to use this site as intended 😊😊
+         </p>
+
+         <p>
+            This website is a MERN Stack project that also showcases my ability
+            to design websites.
+         </p>
+         <h2>Front-end</h2>
+         <ul>
+            <li>React</li>
+            <li>React Router</li>
+            <li>TailwindCSS</li>
+         </ul>
+
+         <h2>Back-end</h2>
+         <ul>
+            <li>Node JS</li>
+            <li>Express</li>
+            <li>MongoDB</li>
+            <li>Mongoose</li>
+            <li>Bcryptjs</li>
+            <li>REST APIs and CRUD</li>
+         </ul>
+
+         <h2>Full-stack</h2>
+         <ul>
+            <li>JWT Authentication and Authorization</li>
+         </ul>
+
+         <p>LinkedIn: </p>
+         <p>GitHub: </p>
+
          <Logout clearData={props.clearData} />
-      </>
+      </div>
    );
 }
